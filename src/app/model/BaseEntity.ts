38cdf26cbb34b7f53c3e325aaa1894826b93054c
Emanueleeee <lelex.ea.ea@gmail.com>
2022-08-01 10:06:34 +0200

    push classi

---
+++ src/app/model/BaseEntity.ts
@@ -0,0 +1,10 @@
+export class BaseEntity {
+  constructor(
+    public id:number,
+    public dataCreazione:Date,
+    public dataModifica:Date,
+    public utenteCreazione:string,
+    public utenteModifica: string
+  ){}
+
+  }
