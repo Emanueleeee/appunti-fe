de475e8f93f869a682d452bcc1653b4faa3b7a54
VprPixo <davidefico0405@gmail.com>
2022-08-01 10:23:14 +0200

    opsie

--- src/app/repositories/RepoUser.ts
+++ src/app/repositories/RepoUser.ts
@@ -3,7 +3,7 @@ import { Injectable } from "@angular/core";
 @Injectable({
     providedIn: 'root'
   })
-    export class UserRepository {
+    export class RepoUser {
 
         constructor(){}
     }
