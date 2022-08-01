04c7d9ce11dcdf76a41b924d86c8b898fbfd0398
Emanueleeee <lelex.ea.ea@gmail.com>
2022-08-01 10:12:28 +0200

    spostato model dentro app

--- src/app/model/BaseEntity.ts
+++ src/app/model/BaseEntity.ts
@@ -1,10 +1,9 @@
 export class BaseEntity {
-  constructor(
-    public id:number,
-    public dataCreazione:Date,
-    public dataModifica:Date,
-    public utenteCreazione:string,
-    public utenteModifica: string
-  ){}
-
-  }
+    constructor(
+      public id:number,
+      public dataCreazione:Date,
+      public dataModifica:Date,
+      public utenteCreazione:string,
+      public utenteModifica: string
+    ){}
+}
--- src/model/BaseEntity.ts
+++
@@ -1,9 +0,0 @@
-export class BaseEntity {
-    constructor(
-      public id:number,
-      public dataCreazione:Date,
-      public dataModifica:Date,
-      public utenteCreazione:string,
-      public utenteModifica: string
-    ){}
-}
