4dc95cbbc252077bc7d352e3cb0ee62e5ffcaa92
Emanueleeee <lelex.ea.ea@gmail.com>
2022-08-02 12:52:11 +0200

    ewfwf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { BoardAdminComponent } from './board-admin/board-admin.component';
-import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
 import { BoardUserComponent } from './board-user/board-user.component';
 
 import { authInterceptorProviders } from './_helpers/auth.interceptor';
@@ -29,7 +28,6 @@ import { CongratulazioniComponent } from './congratulazioni/congratulazioni.comp
     HomeComponent,
     ProfileComponent,
     BoardAdminComponent,
-    BoardModeratorComponent,
     BoardUserComponent,
     AppuntiComponent,
     CongratulazioniComponent
